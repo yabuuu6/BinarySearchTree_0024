b6692974fe77b16dabf01b3f90461073a34dc110
buyaramadhan <ytchochote@gmail.com>
2024-06-14 13:46:06 +0700

    constructor for the node class

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -9,3 +9,12 @@ public:
 	Node* leftchild;
 	Node* rightchild;
 
+	//constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
+
