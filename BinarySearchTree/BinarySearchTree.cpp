8a84953fe086ec46681fd37b4e742444d8ed36ea
buyaramadhan <ytchochote@gmail.com>
2024-06-14 13:47:44 +0700

    membuat search

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -56,4 +56,19 @@ public:
 
 	}
 
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+
+	}
+
 	
