5b78350f77974106d55c0549fca577f3f803037f
buyaramadhan <ytchochote@gmail.com>
2024-06-14 13:48:56 +0700

    membuat postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -101,3 +101,18 @@ public:
 		}
 	}
 
+	void postorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
+};
